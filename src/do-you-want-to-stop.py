1d21519cf8f49fa5f4126a3041b2769c6a2dc9c7
NajaLange <201906591@post.au.dk>
2022-09-06 15:43:32 +0200

    i have commit

--- src/do-you-want-to-stop.py
+++ src/do-you-want-to-stop.py
@@ -1,3 +1,9 @@
 
 # You should write a loop around this question, and keep asking until
 # the answer is "yes" (lower case).
+
+
+x = str(input("Do you want to stop?"))
+
+while x != "yes":
+    x = str(input("Do you want to stop?"))
