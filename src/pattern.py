e1ceaa8e4adb8a0beec2d70208507d4aa0e60cc4
NajaLange <201906591@post.au.dk>
2022-09-06 16:52:00 +0200

    i have commit

--- src/pattern.py
+++ src/pattern.py
@@ -1,2 +1,17 @@
 
 # Print the pattern
+
+
+count = "*"
+count2 = 2
+
+for x in range(1,10):
+    if x == 1:
+        print(count*x)
+    elif x <= 5:
+        print((count + " ")*x)
+    elif x <= 8:
+        print((count + " ")*(x-count2))
+        count2 = count2 + 2 
+    else: 
+        print((count + " ")*(x-count2))
--- src/print-1-10-growing.py
+++ src/print-1-10-growing.py
@@ -6,9 +6,9 @@ from readline import append_history_file
 
 count = ""
 
-for x in range(1,11):
+for x in range(5):
     if x == 1:
-        count = count + str(x)
+        count = count + str(*)
     else:
-        count = count + " " + str(x)
+        count = count + " " + str(*)
     print(count)
