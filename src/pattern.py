263a15227ea4f6437c9deffab1966bb7ecc11865
NajaLange <201906591@post.au.dk>
2022-09-06 17:05:00 +0200

    i have commit

--- src/pattern.py
+++ src/pattern.py
@@ -3,14 +3,14 @@
 
 
 count = "*"
-count2 = 1
+count2 = 2
 
-for x in range(9):
+for x in range(1,10):
     if x == 0:
-        print(count*(x+1))
-    elif x <= 4:
-        print((count + " ")*(x+1))
-    elif x <= 8:
+        print(count*(x))
+    elif x <= 5:
+        print((count + " ")*(x))
+    elif x <= 9:
         print((count + " ")*(x-count2))
         count2 = count2 + 2
     else:
