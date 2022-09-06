2fc48f5ddf045b1b18ed708d6f461c25be167fa6
NajaLange <201906591@post.au.dk>
2022-09-06 16:28:20 +0200

    i have commit

--- src/print-1-10-growing.py
+++ src/print-1-10-growing.py
@@ -7,8 +7,8 @@ from readline import append_history_file
 count = ""
 
 for x in range(1,11):
-    if x != 10:
-        count = count + str(x) + " "
-    else:
+    if x == 1:
         count = count + str(x)
+    else:
+        count = count + " " + str(x)
     print(count)
